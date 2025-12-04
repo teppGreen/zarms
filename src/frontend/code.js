df4b924aae5be0c6ff91102a6e684466b4975b60
TEPPei <tekkun2007.penthouse@gmail.com>
2025-12-04 19:58:10 +0900

    refactor: Introduce `App` namespace and move page-specific data loading into `App.pages.<page_name>.loadData` functions.

--- src/frontend/code.js
+++ src/frontend/code.js
@@ -766,37 +766,3 @@ function searchByCode(code) {
 function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
-
-// ============================================
-// ヘルパー関数: JSONシリアライズ対策
-// (Dateオブジェクトを自動的にISO文字列に変換する)
-// ============================================
-function sanitizeForClient(data) {
-  if (data === null || data === undefined) {
-    return data;
-  }
-
-  // Dateオブジェクトの場合
-  if (data instanceof Date) {
-    return data.toISOString();
-  }
-
-  // 配列の場合は、各要素を再帰的に処理
-  if (Array.isArray(data)) {
-    return data.map(item => sanitizeForClient(item));
-  }
-
-  // プレーンなオブジェクトの場合は、各プロパティを再帰的に処理
-  if (typeof data === 'object' && data.constructor === Object) {
-    const sanitized = {};
-    for (const key in data) {
-      if (Object.prototype.hasOwnProperty.call(data, key)) {
-        sanitized[key] = sanitizeForClient(data[key]);
-      }
-    }
-    return sanitized;
-  }
-
-  // それ以外（文字列、数値、ブール値）はそのまま返す
-  return data;
-}
