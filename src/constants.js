d4162c63a0193aa4e74179a22fc6328ac95bb0a2
TEPPei <tekkun2007.penthouse@gmail.com>
2026-02-03 23:09:46 +0900

    [変更] カレンダータブのスタイルを調整, メンバータブをシートの埋め込みに変更

--- src/constants.js
+++ src/constants.js
@@ -117,9 +117,10 @@ const COLUMN_IDS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
  * 外部URL設定
  */
 const EXTERNAL_URLS = {
-  CALENDAR: 'https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FTokyo&showPrint=0&showTitle=0&mode=MONTH&hl=ja&src=Y182ODEyOWNkMjU3YWE4MDU4YTE3NjAwZjVlYmFlNTg1OWZlOGE3NzEwNGFlZDkxOWQyMWU1OGZjZDQ1NmQ4MTEzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23f09300',
+  ADD_CALENDAR: "https://calendar.google.com/calendar/u/0?cid=Y182ODEyOWNkMjU3YWE4MDU4YTE3NjAwZjVlYmFlNTg1OWZlOGE3NzEwNGFlZDkxOWQyMWU1OGZjZDQ1NmQ4MTEzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20",
+  CALENDAR: 'https://calendar.google.com/calendar/embed?wkst=2&ctz=Asia%2FTokyo&showPrint=0&mode=WEEK&showCalendars=0&showTitle=0&src=Y182ODEyOWNkMjU3YWE4MDU4YTE3NjAwZjVlYmFlNTg1OWZlOGE3NzEwNGFlZDkxOWQyMWU1OGZjZDQ1NmQ4MTEzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23f09300',
+  MEMBER_SPREADSHEET: 'https://docs.google.com/spreadsheets/d/1Jwq5klCWFMNqSURHk2JrbF43X4JTy5I7M5aBE3uDVNw/edit?gid=1456439787',
   CONTACT_FORM: 'https://forms.gle/afXzsGKxvm9qVG4G9',
-  MEMBER_SPREADSHEET: 'https://docs.google.com/spreadsheets/d/1Jwq5klCWFMNqSURHk2JrbF43X4JTy5I7M5aBE3uDVNw/edit',
   SOUMU_DOCUMENT: 'https://docs.google.com/document/d/1BXpWaVsyJChC-rSDgjEvH33s_5FvXiaimvHIYxBTxoY/preview?tab=t.ox00ab3wnxmq',
   LOGO_IMAGE: 'https://lh3.googleusercontent.com/d/1G9u8KOX93-PWEdKUwVWvB7WWGIOuQ2wh',
   ZARMS_IMAGE: 'https://lh3.googleusercontent.com/d/17EMQ6GE9Nu-P7xc2y32rHudx6vy86zi8',
