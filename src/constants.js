266c8baa6d7b63116850f62c553c68ae80f744fc
TEPPei <tepp.green@gmail.com>
2026-02-28 04:48:02 +0900

    membersテーブルを最新の物に更新

--- src/constants.js
+++ src/constants.js
@@ -58,7 +58,7 @@ const TABLE_HEADERS = {
   COMMENTS: ['id', 'display_id', 'content', 'mentioned_to', 'related_table', 'related_id', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
   DIRECTORIES: ['id', 'display_id', 'parent_directory_id', 'directory_type_key', 'name', 'slack_channel_url', 'gfolder_id', 'is_active', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
   APPS: ['id', 'name', 'url', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
-  MEMBERS: ['id', 'email', 'slack_profile_url', 'name', 'display_name', 'profile_photo_url', 'is_active', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
+  MEMBERS: ['id', 'email', 'slack_profile_url', 'name', 'display_name', 'title', 'description', 'top_sns_url', 'profile_photo_file_url', 'profile_photo_url', 'is_active', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
   MEMBER_ASSIGNMENTS: ['id', 'member_id', 'related_table', 'related_id', 'role', 'is_active', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
   TASKS: ['id', 'display_id', 'parent_task_id', 'board_id', 'name', 'description', 'starts_at', 'ends_at', 'task_status_key', 'priority_key', 'processed_by', 'reviewed_by', 'received_by', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
   LISTS: ['id', 'index', 'task_id', 'is_done', 'name', 'assign_to', 'remark', 'created_by', 'created_at', 'updated_by', 'updated_at'],
